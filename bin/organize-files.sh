cb6b3cf319d90a24dd400ead97c49bb509702995
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-02-17 11:27:21 -0500

    feat: check if a directory path was provided as a command argument

---
+++ bin/organize-files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# -z: checks if a string is empty. Returns true if it is. In this case, it's checking if a directory path was provided as an argument
+# if the directory string is empty, it means that we will work in the pwd (the default directory), else we will work in the specified directory
+
+if [ -z $1 ]; then
+    # We store the path of the current directory
+    working_directory=$(pwd)
+else
+    # We store the directory path provided as an argument
+    working_directory=$1
+fi
